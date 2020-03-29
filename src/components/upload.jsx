1bdbea93113797f3420eb0d2fb3bff1975b208d1
Jeanette Yue <yue.jeanette@gmail.com>
2020-03-29 01:14:57 -0700

    implemented upload button

--- src/components/upload.jsx
+++ src/components/upload.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Label, Input } from "@rebass/forms";
 import { Flex, Button } from "rebass";
+import { GlobalContext } from "../context/globalContext";
+
+const handleChange = (files, uploadedImages, setUploadedImages) => {
+  const result = Object.values(files).map(file => URL.createObjectURL(file));
+  setUploadedImages([...uploadedImages, ...result]);
+};
 
 const Upload = () => {
+  const { uploadedImages, setUploadedImages } = useContext(GlobalContext);
+
   return (
     <Flex variant="upload">
       <Input
@@ -11,6 +19,9 @@ const Upload = () => {
         multiple
         accept=".png, .jpg, .jpeg"
         display="none"
+        onChange={e =>
+          handleChange(e.target.files, uploadedImages, setUploadedImages)
+        }
       />
       <Button variant="uploadButton">
         <Label htmlFor="upload" variant="uploadLabel">
